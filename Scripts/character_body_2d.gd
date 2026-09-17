213e5b7860aff853c6632d92bc073f5bef6eb202
Pablo <pablo.beceiro23@gmail.com>
2026-09-17 17:22:29 +0200

    Mejora de la funcionalidad de rockfall

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -1,26 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 200
-const JUMP_SPEED = -600
+const SPEED = 350
+const JUMP_SPEED = -800
 var contadorSaltos = 0
 
 
 func _physics_process(delta: float) -> void:
-	
-	
-	# baja gravedad pulsando control. si no gravedad normal
-	if not is_on_floor() and Input.is_action_pressed("control"):
-		velocity += get_gravity() / 2 * delta
-	else:
+ 	# Add the gravity.
+	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	
-	 #teleport con z (que avance numero de pixeles hacia una direccion)
-	if Input.is_action_just_pressed("z"):
-		position.x += 150
-
-	
-		
 # movimiento en el eje x
 	if Input.is_action_pressed("izquierda"):
 		velocity.x = -SPEED
@@ -28,11 +16,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = SPEED
 	else:
 		velocity.x = 0
-
+		
 # salto del personaje
 	if Input.is_action_just_pressed("espacio") and is_on_floor():
 		contadorSaltos+=1
 		velocity.y = JUMP_SPEED
+#rockfall
+	if(!is_on_floor()) and Input.is_action_pressed("shift"):
+			velocity.x = 0
+			velocity += get_gravity() * 3.5 * delta
+
 		
 		
 		
@@ -46,20 +39,23 @@ func _physics_process(delta: float) -> void:
 			contadorSaltos = 0
 			pass
 		
+ #teleport con z (que avance numero de pixeles hacia una direccion)
+	if Input.is_action_pressed("z"):
+		position.x += 10
 
-#rockfall
-	if(position.y < 0) and Input.is_action_pressed("shift"):
-		
-			velocity.x = 0
-			velocity.y += 9.8 * delta
+# baja gravedad pulsando control
+	if not is_on_floor() and Input.is_action_pressed("control"):
+		velocity += get_gravity() * delta
+			
+
 # wall climb (existe is_on_wall())
 	if(is_on_wall()):
 		velocity.y = JUMP_SPEED
 # sprint con shift pulsado
 	if(Input.is_action_pressed("shift") and Input.is_action_pressed("derecha") and is_on_floor()):
-		velocity.x = SPEED + 150
+		velocity.x = SPEED + 250
 	if(Input.is_action_pressed("shift") and Input.is_action_pressed("izquierda") and is_on_floor()):
-		velocity.x = -SPEED - 150
+		velocity.x = -SPEED - 250
 		
 
 	move_and_slide()
